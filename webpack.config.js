9504e48970c2bd73cbd3a232d53d7754cf92b770
dmee <dmeewg@gmail.com>
2022-03-27 23:00:51 +0900

    webpack에서 src/assets 폴더를 리소스 폴더로 사용하기 위한 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,11 @@ const {VueLoaderPlugin} = require('vue-loader')
 module.exports = {
   resolve: {
     // 확장자를 생략해도 import 잘되게 수정
-    extensions: ['.js', '.vue']
+    extensions: ['.js', '.vue'],
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      'assets': path.resolve(__dirname, 'src/assets')
+    }
   },
   // 파일을 읽어들이기 시작하는 진입점 설정 - webpack은 javascript 파일을 지정
   entry: './src/main.js',
@@ -46,6 +50,10 @@ module.exports = {
         use: [
           'babel-loader'
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: 'file-loader'
       }
     ]
   },
