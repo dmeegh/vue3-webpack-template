b0ced25d2ca9f3c23f87d7d73d7c7e7140e65d6a
dmee <dmeewg@gmail.com>
2022-03-30 23:25:20 +0900

    기본 용도로 변환 - 사용하지 않는 코드 제거 - 아바타로 아이콘 변경

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
-  <h1>{{ message }}</h1>
-  <HelloWorld />
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld';
 
 export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!!!'
-    }
-  }
 }
 </script>
